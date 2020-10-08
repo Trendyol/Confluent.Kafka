b653ff85f257922272509575ea331ea51263fcd2
Dogac <ndakyildiz@gmail.com>
2020-10-08 19:00:43 +0300

    Add Summaries for KafkaConfig.

--- src/Configuration/KafkaConfig.cs
+++ src/Configuration/KafkaConfig.cs
@@ -4,14 +4,42 @@ namespace Confluent.Kafka.Lib.Core.Configuration
 {
     public class KafkaConfig
     {
+        /// <summary>
+        /// Your main topic to be consumed from.
+        /// </summary>
         public string? Topic { get; set; }
+        /// <summary>
+        /// Your retry topic, records are first produced to your retry topic if processing is failed.
+        /// </summary>
         public string? RetryTopic { get; set; }
+        /// <summary>
+        /// If `MaxRetryCount` is exceeded, records are produced to this topic.
+        /// </summary>
         public string? FailedTopic { get; set; }
+        /// <summary>
+        /// If using manual commit, you can set your commit period from here.
+        /// Note that you cannot use CommitPeriod is auto-commit is enabled.
+        /// </summary>
         public int? CommitPeriod { get; set; }
+        /// <summary>
+        /// Maximum retry count for records.
+        /// </summary>
         public int? MaxRetryCount { get; set; }
+        /// <summary>
+        /// Your configuration for main consumer.
+        /// </summary>
         public ConsumerConfig? MainConsumerConfig { get; set; }
+        /// <summary>
+        /// Your configuration for retry consumer.
+        /// </summary>
         public ConsumerConfig? RetryConsumerConfig { get; set; }
+        /// <summary>
+        /// Your configuration for producer for both retry and failed topics.
+        /// </summary>
         public ProducerConfig? RetryProducerConfig { get; set; }
+        /// <summary>
+        /// Set your retry consumer which time period should it periodically run here.
+        /// </summary>
         public TimeSpan? RetryPeriod { get; set; }
     }
 }
