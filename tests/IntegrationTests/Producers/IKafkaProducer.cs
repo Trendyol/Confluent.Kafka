e06d1de31bfe89c143ebec7e87163cf79ae63e1d
Dogac <ndakyildiz@gmail.com>
2021-01-02 23:36:03 +0300

    Remove All Tests.

--- tests/IntegrationTests/Producers/IKafkaProducer.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Confluent.Kafka.Utility.Tests.IntegrationTests.Producers
-{
-    public interface IKafkaProducer
-    {
-        Task ProduceAsync<TKey, TValue>(string topic, Message<TKey, TValue> message);
-    }
-}
